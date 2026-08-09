24c03257462bc2bdf3ca1250cde2c767f4db5b8e
coolenterprisesmw265 <software@coolenterprisesm.com>
2026-08-09 21:26:56 +0200

    Sync authentication fields with Prisma schema

--- prisma/migrations/20260809200000_restore_password_reset_token/migration.sql
+++ prisma/migrations/20260809200000_restore_password_reset_token/migration.sql
@@ -1,2 +1,8 @@
 ALTER TABLE "User"
-ADD COLUMN "passwordResetToken" TEXT;
+ADD COLUMN "emailVerificationExpires" TIMESTAMP(3),
+ADD COLUMN "passwordResetToken" TEXT,
+ADD COLUMN "passwordResetExpires" TIMESTAMP(3),
+ADD COLUMN "resetToken" TEXT,
+ADD COLUMN "resetTokenExpiry" TIMESTAMP(3);
+
+CREATE UNIQUE INDEX "User_resetToken_key" ON "User"("resetToken");
